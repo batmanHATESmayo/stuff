35aa76bcfdbc617bc5ae77da70c1e763a44c42f7
batmanHATESmayo <138705728+batmanHATESmayo@users.noreply.github.com>
2023-07-05 20:11:02 -0700

    Update revenge.bat

--- revenge.bat
+++ revenge.bat
@@ -6,11 +6,15 @@ echo Press 1 for IDA
 echo Press 2 for Ghidra
 echo Press 3 for x32dbg
 echo Press 4 for x64dbg
+echo Press 5 for HxD
+echo Press 6 for Advanced BAT to Exe Converter
 set /p var=
 if %var% == 1 goto ida
 if %var% == 2 goto nsa
 if %var% == 3 goto x32
 if %var% == 4 goto x64
+if %var% == 5 goto hxd
+if %var% == 6 goto b2e
 
 :ida
 cls
@@ -36,6 +40,18 @@ cls
 echo STARTING x64DBG...
 start "" "C:\Program Files\snapshot_2023-06-15_13-51\release\x64\x64dbg.exe"
 goto middle
+
+:hxd
+cls
+echo STARTING HxD...
+start "" "C:\Program Files\HxD\HxD.exe"
+goto eof
+
+:b2e
+cls
+echo STARTING Advanced BAT to Exe Converter...
+start "" "C:\Program Files (x86)\Advanced BAT to EXE Converter v4.59\ab2econv459\aB2Econv.exe" 
+goto eof
 
 :middle
 echo.
